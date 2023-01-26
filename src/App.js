cfa6af2ce220d8f012f16c499165204baa69c234
auqid <auqidirfan100@gmail.com>
2023-01-26 16:12:21 +0530

    github extension ko lanath

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
       date: new Date(2020, 7, 14),
     },
     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
+    { 
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
