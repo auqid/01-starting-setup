47eccfdd2d066e73ba7da12fc185e8c6a020a382
auqid <38567390+auqid@users.noreply.github.com>
2023-01-31 18:26:45 +0530

    added a chart in expense tracker using conditionals

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React from "react";
+import '/Chart.css';
 
-import ChartBar from './ChartBar';
-import './Chart.css';
 
-const Chart = (props) => {
-  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
-  const totalMaximum = Math.max(...dataPointValues);
+const Chart = props => {
+    const dataPointsValues = props.dataPoints.map(dataPoint=> dataPoint.value);
+    const totalMaximum = Math.max(...dataPointsValues);
+    return(
+    <div className="chart">
+        {props.dataPoints.map(dataPoint => <ChartBar 
+        key={dataPoint.label}
+        value={dataPoint.value}
+        maxValue ={totalMaximum}
+        label={dataPoint.label} />)}
 
-  return (
-    <div className='chart'>
-      {props.dataPoints.map((dataPoint) => (
-        <ChartBar
-          key={dataPoint.label}
-          value={dataPoint.value}
-          maxValue={totalMaximum}
-          label={dataPoint.label}
-        />
-      ))}
     </div>
-  );
+    );
+
 };
 
 export default Chart;
